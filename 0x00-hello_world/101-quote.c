7ee7df960b842521eedc0f982d88b7b100f47938
hazemali1 <hazemali1343@gmail.com>
2023-03-16 19:43:48 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
 * main - Entry point
 *
 * Description: print a qoute using write function
-*       write(inf fd.const void *buf ,size_t count)
+*      ssize_t write(int fd, const void *buf, size_t count);
 *
-*       Return :1 (not success)
+*       Return: 1 (error)
 */
+
+
 int main(void)
-{
-	char you[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char you[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, you, 59);
 	return (1);
