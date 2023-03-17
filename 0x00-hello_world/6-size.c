548a23ebe0b9f4b9c1406575378c849c1d71237b
hazemali1 <hazemali1343@gmail.com>
2023-03-16 17:56:25 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: A c program to printing
+ *     *
+ *      * Return: Always 0 mean it (sucess)
+ *      */
+int main(void)
+{
+	int s;
+	float d;
+	char f;
+	long long g;
+	printf("Size of a char: %lu.\n"(unsigned long)sizeof(f));
+	printf("Size of an int: %lu.\n"(unsigned long)sizeof(s));
+	printf("Size of a long int: %lu.\n"(unsigned long)sizeof(g));
+	printf("Size of a long long int: %lu.\n"(unsigned long)sizeof(g));
+	printf("Size of a float: %lu.\n"(unsigned long)sizeof(d));
+		return (0);
+}
