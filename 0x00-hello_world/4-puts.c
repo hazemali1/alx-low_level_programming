b2656037488d1c5543ca84454c94aa3d1e66ac46
hazemali1 <hazemali1343@gmail.com>
2023-03-16 17:17:11 -0700

    gcc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A c program to printing
+ *
+ * Return: Always 0 mean it (sucess)
+*/
 int main(void)
 {
-/*
- * puts
- */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
