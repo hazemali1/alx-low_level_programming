429fd47d0dd4fd761fe1d01deaa92185dc69c427
hazemali1 <hazemali1343@gmail.com>
2023-03-17 15:49:23 -0700

    AZ

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Get
+ *
+ * Description: Alpha
+ *
+ * Return: Returns 0 (sucess)
+*/
+int main(void)
+{
+	int x, s;
+for (x = 97; x <= 122; x++)
+{
+	putchar(x);
+}
+for (s = 65; s <= 90; s++)
+{
+	putchar(s);
+}
+	putchar('\n');
+	return (0);
+}
