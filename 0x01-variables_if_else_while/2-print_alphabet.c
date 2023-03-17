48f4ea49eb5b888c241956a271f98f3a31b03bd5
hazemali1 <hazemali1343@gmail.com>
2023-03-17 15:22:44 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Get
+ *
+ * Description: Alpha
+ *
+ * Return: Returns 0 (sucess)
+*/
+int main(void)
+{
+	int x;
+for (x = 97; x <= 122; x++)
+{
+	putchar(x);
+}
+	return (0);
+}
