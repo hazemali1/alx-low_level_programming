9565105007c089d87127bb9b74bf301a251c9e79
hazemali1 <hazemali1343@gmail.com>
2023-03-17 17:15:36 -0700

    rev alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Get
+ *
+ * Description: rev
+ *
+ * Return: Returns 0 (sucess)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 122; x >= 97; x--)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
