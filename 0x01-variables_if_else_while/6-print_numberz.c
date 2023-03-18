acb3e989fb1893846c079178561c99966b833ce8
hazemali1 <hazemali1343@gmail.com>
2023-03-17 17:09:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Numberz
+ *
+ * Description: Ascii
+ *
+ * Return: Returns 0 (sucess)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
