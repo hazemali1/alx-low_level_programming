901c2aced28de853cdff02c1ea03213347adfe62
hazemali1 <hazemali1343@gmail.com>
2023-03-21 12:46:30 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks
+ * c - Parameter
+ *
+ * Description: Checks alphabet
+ *
+ * Return: Return 0 (sucess)
+*/
+int _islower(int c)
+	if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+	else if (c >= 65 && c <= 90)
+{
+	return (0);
+}
