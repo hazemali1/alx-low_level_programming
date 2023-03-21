3ccb833e54f544bed5f7dad3295fa66a17ca6a4e
hazemali1 <hazemali1343@gmail.com>
2023-03-21 16:49:42 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print
+ *
+ * Description: Print last digit
+ *
+ * @s: Pramater
+ *
+ * Return: Return 0 (sucess)
+*/
+int print_last_digit(int s)
+{
+	s = s % 10;
+	return (s);
+}
