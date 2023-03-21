1856622c477cc5d772f79d89fd4bb49943c0f3e7
hazemali1 <hazemali1343@gmail.com>
2023-03-21 15:40:21 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _abs - Absolute
+  *
+  * Description: Absolute
+  *
+  * @s: Parameter
+  *
+  * Return: Return 0 (sucess)
+ */
+int _abs(int s)
+{
+	if (s < 0)
+	{
+		s = s * -1;
+	}
+	else
+	{
+		s = s;
+	}
+	return (_abs(0));
+}
