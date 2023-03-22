766fe90e27408155db98a527b12876474f5bff0d
hazemali1 <hazemali1343@gmail.com>
2023-03-21 17:40:47 -0700

    readme

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,21 @@ void jack_bauer(void)
 	int s, d, f, g;
 
 for (s = 0; s <= 2; s++)
-for (d = 0; d <= 3; d++)
-for (f = 0; f <= 5; f++)
-for (g = 0; g <= 9; g++)
+{
+	for (d = 0; d <= 3; d++)
+	{
+		for (f = 0; f <= 5; f++)
+		{
+			for (g = 0; g <= 9; g++)
+			{
 _putchar(s + '0');
 _putchar(d + '0');
 _putchar(':');
 _putchar(f + '0');
 _putchar(g + '0');
 _putchar('\n');
+}
+}
+}
+}
 }
