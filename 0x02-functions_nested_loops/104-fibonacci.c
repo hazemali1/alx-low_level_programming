9ac7a143842ae93c64daf215fe98d1a80d0c9a5b
hazemali1 <hazemali1343@gmail.com>
2023-03-22 07:22:54 -0700

    100

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,20 @@
 */
 int main(void)
 {
-	long long int s = 1;
-	long long int d = 2;
-	long long int f, g, h;
+	long int s = 1;
+	long int d = 2;
+	long int f, g, h;
 
-	printf("%lld, ", s);
-	printf("%lld, ", d);
+	printf("%ld, ", s);
+	printf("%ld, ", d);
 	for (g = 0; g < 48; g++)
 	{
 	f = s + d;
 	h = f;
-	printf("%lld, ", f);
+	printf("%ld, ", f);
 	f = h + d;
 	d = f;
-	printf("%lld", d);
+	printf("%ld", d);
 	if (g < 47)
 	{
 	printf(", ");
