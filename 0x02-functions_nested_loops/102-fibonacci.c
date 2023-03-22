0d82b6f9b7500691c7f43cf5d34152c945a478f5
hazemali1 <hazemali1343@gmail.com>
2023-03-21 19:36:58 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program to printing
+ *
+ * Return: Always 0 mean it (sucess)
+*/
+int main(void)
+{
+	int s = 1;
+	int d = 2;
+
+	printf("%d, ", s, d);
+	f = s + d;
+	printf("%d, ", f);
+	return (0);
+}
