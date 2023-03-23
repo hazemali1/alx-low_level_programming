9c84c4f61dad1e245c3d5b09badb3dcce85b734e
hazemali1 <hazemali1343@gmail.com>
2023-03-23 12:24:13 -0700

    exceps digit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print
+ *
+ * Description: Print except
+ *
+ * Return: Return 0 (sucess)
+*/
+void print_most_numbers(void)
+{
+	int s;
+
+	_putchar(48);
+	_putchar(50);
+	for (s >= 53; s <= 57; s++)
+	{
+		_putchar(s);
+	}
+}
