3dc7a4452ea3ed9e5db7078e688f3672d41eea0d
hazemali1 <hazemali1343@gmail.com>
2023-03-23 15:02:39 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Print
+ *
+ * Description: Print line
+ *
+ * @n: Parameter
+ *
+ * Return: Returns 0 (sucess)
+*/
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	for (s = 0; s <= n; s++)
+	{
+		_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
