69f0ba01604babff42ff1944a11a243d70d57a66
hazemali1 <hazemali1343@gmail.com>
2023-03-23 12:10:09 -0700

    print digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print
+ *
+ * Description: Digit
+ *
+ * Return: Returns 0 (sucess)
+*/
+void print_numbers(void)
+{
+	int s;
+
+	for (s = 48; s <= 57; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
