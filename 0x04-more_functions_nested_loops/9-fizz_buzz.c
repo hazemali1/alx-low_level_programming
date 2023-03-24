dd9333ea59dfa26d63c6844123a464740983b1fa
hazemali1 <hazemali1343@gmail.com>
2023-03-23 17:10:38 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * Description: fizz buzz
+ *
+ * Return: Returns 0 (sucess)
+*/
+int main(void)
+{
+	int s;
+
+	for (s = 1; s <= 100; s++)
+	{
+		if (s % 3 == 0 && s % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (s % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (s % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", s);
+		}
+	}
+	printf("\n");
+	return (0);
+}
