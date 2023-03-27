5b154f0905817f260080bd2293f384bdfae043e4
hazemali1 <hazemali1343@gmail.com>
2023-03-27 14:37:40 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Prinnt
+ *
+ * Description: Rev
+ *
+ * @s: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+void rev_string(char *s)
+{
+        int f, d = 0;
+
+        while (s[d] != '\0')
+        {
+                d++;
+        }
+        for (f = d - 1; f >= 0; f--)
+        {
+		s[] = s[f];
+        }
+}
