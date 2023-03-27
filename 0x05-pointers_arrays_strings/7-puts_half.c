d24afd5eea364c92ba67aa42ea9ff9bbdcb923ca
hazemali1 <hazemali1343@gmail.com>
2023-03-27 16:56:58 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Print
+ *
+ * Description: Print half
+ *
+ * @str: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+void puts_half(char *str)
+{
+	int d = 0, f;
+
+	while (*str != '\0')
+	{
+		str++;
+		d++;
+	}
+	f = d / 2;
+	while (f <= d)
+	{
+		_putchar(str[f]);
+		f++;
+	}
+	_putchar('\n');
+}
