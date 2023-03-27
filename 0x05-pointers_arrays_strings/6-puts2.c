fceb1f318cb41264eca9f7705b79a10e66f82153
hazemali1 <hazemali1343@gmail.com>
2023-03-27 16:16:11 -0700

    next one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prinnt
+ *
+ * Description: Rev
+ *
+ * @str: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+	_putchar(*str);
+	str = str + 2;
+}
+}
