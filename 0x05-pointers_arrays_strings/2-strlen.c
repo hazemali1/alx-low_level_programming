897f7fb93190c1f5ac0f0aff7f5f058b41ceaf41
hazemali1 <hazemali1343@gmail.com>
2023-03-27 11:52:33 -0700

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Lenght
+ *
+ * Description: Lenght
+ *
+ * @s: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+int _strlen(char *s)
+{
+	strlen(s);
+	return (s);
+}
