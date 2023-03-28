537a20e6267388f153cfb63f6116c4e732bccc69
hazemali1 <hazemali1343@gmail.com>
2023-03-27 18:58:01 -0700

    int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,17 +11,16 @@
 */
 int _atoi(char *s)
 {
-	int d = 0, e = 0, a[1000];
+	int d = 0, e = 0;
 
 	while (s[d] != '\0')
 	{
 		if (s[d] >= 0 && s[d] <= 9)
 		{
-			a[e] = s[d];
+			s[e] = s[d];
 		}
 	d++;
 	e++;
 	}
-	s[e] = a[e];
 	return (0);
 }
