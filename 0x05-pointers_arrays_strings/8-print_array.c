6252e9bb22b7b6bf8a24cb0e43416ae748cbe291
hazemali1 <hazemali1343@gmail.com>
2023-03-27 17:43:21 -0700

    element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Print
+ *
+ * Description: Prints
+ *
+ * @a: Parameter
+ * @n: Parameters
+ *
+ * Return: Return 0 (sucess)
+*/
+void print_array(int *a, int n)
+{
+	printf("%d, ", a[n]);
+	Printf("\n");
+}
