cf94c865eca1da103f1884ecbed4b78535288f57
hazemali1 <hazemali1343@gmail.com>
2023-03-27 18:09:00 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies
+ *
+ * Description: Copies
+ *
+ * @dest: Parameter
+ * @src: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (src);
+}
