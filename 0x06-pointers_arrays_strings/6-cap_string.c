ac6452330cc0d3795c2ce07662dac3d5fd9ffa52
hazemali1 <hazemali1343@gmail.com>
2023-03-30 14:31:49 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,21 @@
 */
 char *cap_string(char *str)
 {
-    int s = 0;
+	int s = 0;
 
 while (str[s] != '\0')
 {
-	if ((str[s] == ' ' || str[s] == '\n' || str[s] == ',' || str[s] == '\t' || str[s] == ';' || str[s] == '.' || str[s] == '!' || str[s] == '?' || str[s] == '"' || str[s] == '(' || str[s] == ')' || str[s] == '{' || str[s] == '}') && (str[s + 1] >= 97 && str[s + 1] <= 122))
+	if (str[s] >= 32 && str[s] <= 44  || str[s] == 59 || str[s] == 63 || str[s] == '{' || str[s] == '}' || str[s] == '.' || str[s] == '\n' || str[s] == '\t')
 	{
-		str[s] = str[s];
-		str[s + 1] = str[s + 1] - 32;
+		if (str[s + 1] >= 97 && str[s + 1] <= 122)
+		{
+			str[s] = str[s];
+			str[s + 1] = str[s + 1] - 32;
+		}
+		else
+		{
+			str[s] = str[s];
+		}
 	}
 	else
 
