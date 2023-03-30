ef9a77976f6a50bebc3e3b3f50e9487a5a749542
hazemali1 <hazemali1343@gmail.com>
2023-03-30 13:05:07 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * reverse_array - Rev
+ *
+ * Description: Rev
+ *
+ * @a: Parameter
+ * @n: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+void reverse_array(int *a, int n)
+{
+	int e[n];
+	int s;
+	int r = n;
+
+	for (s = 0; s < r; s++)
+	{
+		e[s] = a[s];
+	}
+	for (s = 0; s < r; s++)
+	{
+		a[s] = e[n - 1];
+		n--;
+	}
+}
