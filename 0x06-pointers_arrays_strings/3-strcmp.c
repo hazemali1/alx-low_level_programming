e20d7cd6655750edc1861073cc0e66ac3f533a5e
hazemali1 <hazemali1343@gmail.com>
2023-03-30 11:07:29 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int d, f = 0, r = 0, o = 0;
+	int d, f = 0, r = 0, o = 0, q = 0;
 
-	while (s1[f] != '\0')
+	while (s1[f] != '\0' && s2[q] != '\0')
 	{
 	if (s1[r] == s2[o])
 	{
@@ -26,6 +26,7 @@ int _strcmp(char *s1, char *s2)
 		d = s1[r] - s2[o];
 	}
 	f++;
+	q++;
 	}
 	return (d);
 }
