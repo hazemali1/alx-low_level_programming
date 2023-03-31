0c1493934ed30c6303bb20e7e14526e0abd6c754
hazemali1 <hazemali1343@gmail.com>
2023-03-30 17:32:10 -0700

    print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Print
+ *
+ * Description: Print
+ *
+ * @n: Parameter
+ *
+ * Return: Return 0 (sucess)
+*/
+void print_number(int n)
+{
+	printf("%d", n);
+}
