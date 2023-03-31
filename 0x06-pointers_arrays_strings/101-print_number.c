dd527c8157c65a66806b80524b09b85cec0d7f72
hazemali1 <hazemali1343@gmail.com>
2023-03-30 18:00:45 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,14 +12,17 @@
 */
 void print_number(int n)
 {
+	unsigned int s;
+
+	s = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
-	if (n / 10 != 0)
+	if (s / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(s / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((s % 10) + '0');
 }
