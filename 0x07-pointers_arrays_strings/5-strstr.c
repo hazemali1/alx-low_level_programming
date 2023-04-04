8f1c3a1d457308f3f049885c88cabab2db5a63e1
hazemali1 <hazemali1343@gmail.com>
2023-04-03 18:35:08 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strstr - Function
+ *
+ * Description: Compare
+ *
+ * @haystack: Parameter1
+ * @needle: Parameter2
+ *
+ * Return: Return haystack
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int d = 0, w = 0;
+
+	while (haystack[w] != '\0')
+	{
+		int q = 0;
+
+		while (needle[q] != '\0')
+		{
+			int l = w, o = 0;
+
+			while (haystack[l] == needle[o])
+			{
+				if (needle[o] != '\0')
+				{
+					l++;
+					o++;
+				}
+				else
+				{
+					return (haystack + d);
+				}
+			}
+			q++;
+		}
+		w++;
+		d++;
+	}
+	return ('\0');
+}
