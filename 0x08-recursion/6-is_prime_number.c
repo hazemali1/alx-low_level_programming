a8057d628b7474d3d64f2a6b72a87bf495daaffc
hazemali1 <hazemali1343@gmail.com>
2023-04-05 18:43:43 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * loop - Loop
+ *
+ * @s: Parameter1
+ * @d: Parameter2
+ *
+ * Return: Return 1,0
+*/
+int loop(int s, int d)
+{
+	if (s < 0)
+	{
+		s = s * -1;
+	}
+	if (s == 0 || s == 1)
+	{
+		return (0);
+	}
+	if (s % d != 0 && s > d)
+	{
+		return (loop(s, d + 1));
+	}
+	else if (s <= d)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * is_prime_number - Calling loop
+ *
+ * @n: Parameter
+ *
+ * Return: Return loop
+*/
+int is_prime_number(int n)
+{
+	return (loop(n, 2));
+}
