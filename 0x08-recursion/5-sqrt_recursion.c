0e68d2b2410c5990f24fc4e0cb39232826688c11
hazemali1 <hazemali1343@gmail.com>
2023-04-05 17:45:41 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * tired - Tired
+ *
+ * @s: Parameter1
+ * @d: Parameter2
+ *
+ * Return: Return sqrt
+*/
+int tired(int s, int d)
+{
+	if (s == d * d)
+	{
+		return (d);
+	}
+	else if (s > d * d)
+	{
+		tired(s, d + 1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
+/**
+ * _sqrt_recursion - Sqrt
+ *
+ * @n: Parameter1
+ *
+ * Return: Return tired
+*/
+int _sqrt_recursion(int n)
+{
+	tired(n, d + 1);
+}
