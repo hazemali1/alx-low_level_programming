32ba38c258608b23ef943785c7fed7dcc7de6db1
hazemali1 <hazemali1343@gmail.com>
2023-04-07 01:59:21 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * @argc: Parameter1
+ * @argv: Parameter2
+ *
+ * Return: Return 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
