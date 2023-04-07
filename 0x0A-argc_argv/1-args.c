f8ff7b1de4a717df6aede597f7713f42f6f21793
hazemali1 <hazemali1343@gmail.com>
2023-04-07 02:24:33 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Print
+ *
+ * @argc: Parameter1
+ * @argv: Parameter2
+ *
+ * Return: Return argc
+*/
+int main(int argc, char *argv[])
+{
+	printf("%D\n", argc - 1);
+	return (0);
+}
