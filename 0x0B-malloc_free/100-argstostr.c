7db3582d0b7df256a5ce553cb7d4c5bfe7118215
hazemali1 <hazemali1343@gmail.com>
2023-04-12 16:20:36 -0700

    stostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,25 @@
 #include "main.h"
 
 /**
+ * lenght - Lenght
+ *
+ * @str: Parameter
+ *
+ * Return: Return lenght
+*/
+int lenght(char *str)
+{
+	int d = 0;
+
+	while(*str != '\0')
+	{
+		d++;
+		str++;
+	}
+	return (d);
+}
+
+/**
  * argstostr - Concat
  *
  * @ac: Parameter1
@@ -11,21 +30,39 @@
 */
 char *argstostr(int ac, char **av)
 {
-	int d;
-	char **s;
+	int d, q = 0;
+	char *s;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	s = malloc(sizeof(char *) * ac);
+	for (d = 0; d < ac; d++)
+	{
+		q = q + (lenght(av[d]));
+	}
+	s = malloc(sizeof(char) * q + ac + 1);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 	for (d = 0; d < ac; d++)
 	{
-		s[d] = av[d][0];
+		for (e = 0; e <= (lenght(av[d])); e++)
+		{
+			for (r = 0; r < (q + ac); r++)
+			{
+				if (e == lenght(av[d]))
+				{
+					s[r] = '\n';
+				}
+				else
+				{
+					s[r] = av[d][e];
+				}
+			}
+		}
 	}
+	s[r] = '\0';
 	return (s);
 }
