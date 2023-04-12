e599688aa9ca9d8e47bfd5c16e9ad16f18116261
hazemali1 <hazemali1343@gmail.com>
2023-04-12 13:20:52 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_grid - Free
+ *
+ * @grid: Parameter1
+ * @height: Parameter2
+*/
+void free_grid(int **grid, int height)
+{
+	int d;
+
+	for (d = 0; d < height; d++)
+	{
+		free(grid[d]);
+	}
+	free(grid);
+}
