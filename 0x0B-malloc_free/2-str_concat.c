5524ee8bbda2d1f278c807a6da038f0f19191b62
hazemali1 <hazemali1343@gmail.com>
2023-04-11 18:55:42 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,19 +14,15 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int d = 0, e, w = 0, u = 0;
 
-	while (s1[d] >= '\0')
+	while (s1[d] != '\0')
 	{
 		d++;
 	}
-	while (s2[w] >= '\0')
+	while (s2[w] != '\0')
 	{
 		w++;
 	}
 	s = malloc((sizeof(*s) * (d + w)) + 1);
-	if (s == NULL)
-	{
-		return (NULL);
-	}
 	for (e = 0; e < (d + w); e++)
 	{
 		if (e < d)
