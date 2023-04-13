2afa8055a56e91ee4d1eff1a7da0ae7c704d3634
hazemali1 <hazemali1343@gmail.com>
2023-04-13 09:00:01 -0700

    contact

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * lenght - Lenght
+ *
+ * @s1: Parameter
+ *
+ * Return: Return lenght
+*/
+int lenght(char *s1)
+{
+	int s = 0;
+
+	while (s1[s] != '\0')
+	{
+		s++;
+	}
+	return (s);
+}
+
+/**
+ * string_nconcat - Concatenates
+ *
+ * @s1: Parameter1
+ * @s2: Parameter2
+ * @n: Parameter3
+ *
+ * Return: Return Pointer
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	int d = lenght(s1);
+	int e, r = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+	s = malloc((sizeof(*s) * (n + d)) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (e = 0; e < (n + d); e++)
+	{
+		if (e < d)
+		{
+			s[e] = s1[e];
+		}
+		else if (r <= n)
+		{
+			s[e] = s2[r];
+			r++;
+		}
+	}
+	s[e] = '\0';
+	return (s);
+}
