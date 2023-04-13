c7d94116aef2f25acb1ab43ba768b942fcd5d289
hazemali1 <hazemali1343@gmail.com>
2023-04-13 08:08:37 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Malloc
+ *
+ * @b: Parameter
+ *
+ * Return: Return Pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (b == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+}
