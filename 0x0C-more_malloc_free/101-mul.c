6e8304fed59ed12a15fe725ab1e20417f214d79f
hazemali1 <hazemali1343@gmail.com>
2023-04-13 19:26:48 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,67 +1,52 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print - print
  *
  * @s: Parameter
+ *
+ * Return: Return d
 */
-void print(char *s)
+int print(char *s)
 {
 	int d = 0;
 
-	while (s[d])
+	while (s[d] != '\0')
 	{
-		_putchar(s[d]);
 		d++;
 	}
+	return (d);
 }
 
 /**
- * atoi - Atoi
- *
- * @s: Parameter
- *
- * Return: Return digit
+ * error - Error
 */
-int atoi(const char *s)
+void error(void)
 {
-	int d = 1;
-	unsigned long int w = 0, q, e;
-
-	for (q = 0; !(s[q] >= 48 && s[q] <= 57); q++)
-	{
-		if (s[q] == '-')
-		{
-			d = d * -1;
-		}
-	}
-	for (e = q; s[e] >= 48 && s[e] <= 57; e++)
-	{
-		w = w * 10;
-		w = w + (s[e] - 48);
-	}
-	return (d * w);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * printidgit - printing
+ * _isdigit - Is
  *
  * @s: Parameter
  *
- * Return: Return 0
+ * Return: Return 0,1
 */
-void printidgit(unsigned long int s)
+int _isdigit(char *s)
 {
-	unsigned long int d = 1, w, q;
+	int i = 0;
 
-	for (w = 0; (s / d) > 9; w++, d = d * 10)
-	;
-	for (; d >= 1; s = s % d, d = d / 10)
+	while (s[i])
 	{
-		q = s / d;
-		_putchar('0' + q);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 /**
@@ -72,16 +57,46 @@ void printidgit(unsigned long int s)
  *
  * Return: Return 0
 */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !_isdigit(s1) || !_isdigit(s2))
+		error();
+	len1 = print(s1);
+	len2 = print(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len--)
 	{
-		print("Error ");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = print(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry = carry + result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry = carry / 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	printidgit(atoi(argv[1]) * atoi(argv[2]));
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('\0');
 	_putchar('\n');
+	free(result);
 	return (0);
 }
