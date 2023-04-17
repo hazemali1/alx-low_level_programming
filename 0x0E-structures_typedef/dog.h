d53da2dd34fc0d8aae00964744de494936e1703a
hazemali1 <hazemali1343@gmail.com>
2023-04-17 14:44:05 -0700

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct
+ *
+ * @name: Parameter1
+ * @age: Parameter2
+ * @owner: Parameter3
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
