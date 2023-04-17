2a273fa9f6226c76e4a4d602ea9b083dd002b4e4
hazemali1 <hazemali1343@gmail.com>
2023-04-17 14:51:07 -0700

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Init
+ *
+ * @d: Parameter1
+ * @name: Parameter2
+ * @age: Parameter3
+ * @owner: Parameter3
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->\name;
+	d->age;
+	d->owner;
+}
