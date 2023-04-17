191fe29863af7c19a2cb01de1b0e3d650b3bbe29
hazemali1 <hazemali1343@gmail.com>
2023-04-17 09:07:22 -0700

    file's name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Main
+ *
+ * @argc: Parameter1
+ * @argv: Parameter2
+ *
+ * Return: Return 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
