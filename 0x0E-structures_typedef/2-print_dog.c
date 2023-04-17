ad2ab1af59273b4a3406dea06143accc13fde973
hazemali1 <hazemali1343@gmail.com>
2023-04-17 16:41:11 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("(nil)");
+			printf("(nil)\n");
 		}
 		else
 		{
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		}
 		if (d->age == '\0')
 		{
-			printf("(nil)");
+			printf("(nil)\n");
 		}
 		else
 		{
@@ -28,7 +28,7 @@ void print_dog(struct dog *d)
 		}
 		if (d->owner == NULL)
 		{
-			printf("(nil)");
+			printf("(nil)\n");
 		}
 		else
 		{
