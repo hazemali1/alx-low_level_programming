6c99f65bf6e9aebc1656c2dc8d0ef48552fb6b22
hazemali1 <hazemali1343@gmail.com>
2023-04-17 10:03:27 -0700

    macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,20 +1,4 @@
 #ifndef MACRO_H
 #define MACRO_H
-include "main.h"
-
-/**
- * ABS - Absolute
- *
- * @x: Parameter
- *
- * Return: Return absoilute
-*/
-int ABS(int x)
-{
-	if (x < 0)
-	{
-		x = x * (-1);
-	}
-	return (x);
-}
+#define ABS(x) ((x) * (-1))
 #endif
