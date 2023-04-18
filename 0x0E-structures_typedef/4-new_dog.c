89523d49bf2bb75e94df52f8e305046374e10fe2
hazemali1 <hazemali1343@gmail.com>
2023-04-17 17:29:34 -0700

    copy

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Copy
+ *
+ * @name: Parameter1
+ * @age: Parameter2
+ * @owner: Parameter3
+ *
+ * Return: Return pointer
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *s;
+
+	s = malloc(sizeof(dog_t));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	s->name = name;
+	s->owner = owner;
+}
