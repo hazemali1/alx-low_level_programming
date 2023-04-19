b6dc51eb1ded7441180d6683337827ca5345f896
hazemali1 <hazemali1343@gmail.com>
2023-04-19 07:33:42 -0700

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes
+ *
+ * @array: Parameter1
+ * @size: Parameter2
+ * @action: Parameter3
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int s;
+
+	for (s = 0; s < size; s++)
+	{
+		(*action)(array[s]);
+	}
+}
