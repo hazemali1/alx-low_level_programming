b66cd65355038bf1ae14c85f88193cc4992bba39
hazemali1 <hazemali1343@gmail.com>
2023-04-19 10:36:14 -0700

    program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - Main
+ *
+ * @argc: Parameter1
+ * @argv: Parameter2
+ * Return: Return 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*d)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	d = get_op_func(argv[2]);
+	if (!d)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' && *argv[3] == 0) || (*argv[2] == '%' && *argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", d(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adding
+ *
+ * @a: Parameter1
+ * @b: Parameter2
+ *
+ * Return: Return add
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ *
+ * @a: Parameter1
+ * @b: Parameter2
+ *
+ * Return: Return sub
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul
+ *
+ * @a: Parameter1
+ * @b: Parameter2
+ *
+ * Return: Return mul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ *
+ * @a: Parameter1
+ * @b: Parameter2
+ *
+ * Return: Return div
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod
+ *
+ * @a: Parameter1
+ * @b: Parameter2
+ *
+ * Return: Return mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
