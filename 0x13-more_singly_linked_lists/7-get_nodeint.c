a6720d6034c51528a5494f6dc67f184f0b16ec69
hazemali1 <hazemali1343@gmail.com>
2023-05-02 04:49:17 -0700

    Node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Get index
+ *
+ * @head: Parameter1
+ * @index: Parameter2
+ *
+ * Return: Node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *s;
+	int d;
+
+	s = head;
+	for (d = 0; d < index; d++)
+	{
+		s = s->next;
+	}
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	return (s);
+}
