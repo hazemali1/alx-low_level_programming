79500f0b0ec73f7f3d4dd2f284e4a11c13d87ad4
hazemali1 <hazemali1343@gmail.com>
2023-05-02 19:54:54 -0700

    free

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,50 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop - Finding
+ *
+ * @head: Parameter
+ *
+ * Return: Size
+ */
+int *find_listint_loop(listint_t *head)
+{
+        listint_t *s, *d;
+	int w = 0;
+
+        if (head == NULL)
+        {
+                return (w);
+        }
+        d = head;
+        d = d->next;
+	w++;
+        while (d != NULL)
+        {
+                if (d == d->next)
+                {
+                        return (w);
+                }
+                s = head;
+                while (s != d)
+                {
+                        if (s == d->next)
+                        {
+                                return (s);
+                        }
+			else
+			{
+				w++;
+                        	s = s->next;
+			}
+                }
+		w = 0;
+                d = d->next;
+        }
+        return (NULL);
+}
+
+/**
  * free_listint_safe - Free
  *
  * @h: Parameter
@@ -11,9 +55,11 @@ size_t free_listint_safe(listint_t **h)
 {
 	listint_t *s, *d;
 	size_t w = 0;
+	int q;
 
+	q = find_listint_loop(*h);
 	s = *h;
-	while (s != NULL && w < 8)
+	while (s != NULL && w < q)
 	{
 		d = s->next;
 		s->next = NULL;
