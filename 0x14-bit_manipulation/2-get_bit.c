c8a9c979771e9bd083b761d72b2d78f2185574c5
hazemali1 <hazemali1343@gmail.com>
2023-05-04 16:05:12 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_bit - Get bit
+ *
+ * @n: Parameter1
+ * @index: Parameter2
+ *
+ * Return: 1, 0
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int s, d = 1;
+	unsigned long int w = n;
+
+	while (w / 10 != 0)
+	{
+		w /= 10;
+		d++;
+	}
+	s = 4 * d;
+	if (s <= index)
+	{
+		return (-1);
+	}
+	if (n & 1 << index)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
