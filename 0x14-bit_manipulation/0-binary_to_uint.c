71f2d990107497a55f22b883b4c9e354e04d0511
hazemali1 <hazemali1343@gmail.com>
2023-05-04 08:29:50 -0700

    convert

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Conert
+ *
+ * @b: Parameter
+ *
+ * Return: Number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int s = 0, w = 1;
+	int d = 0, r;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[d] != '\0')
+	{
+		d++;
+	}
+	d--;
+	for (r = d; r >= 0; r--)
+	{
+		if (b[r] != '1' && b[r] != '0')
+		{
+			return (0);
+		}
+		s += (b[r] - 48) * w;
+		w *= 2;
+	}
+	return (s);
+}
