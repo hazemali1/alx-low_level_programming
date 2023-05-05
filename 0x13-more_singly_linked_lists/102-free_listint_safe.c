c151ff90580c9fd1c681e377c95d0bf2f1cfe1c8
hazemali1 <hazemali1343@gmail.com>
2023-05-05 13:55:54 -0700

    free safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,46 @@
 #include "lists.h"
 
 /**
+ * find_size_loop - Size
+ *
+ * @head: Parameter
+ *
+ * Return: Size
+*/
+size_t find_size_loop(listint_t *head)
+{
+	listint_t *s, *d;
+	size_t w = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	d = head;
+	d = d->next;
+	while (d != NULL)
+	{
+		if (d == d->next)
+		{
+			return (w);
+		}
+		s = head;
+		while (s != d)
+		{
+			if (s == d->next)
+			{
+				w++;
+				return (w);
+			}
+			s = s->next;
+		}
+		d = d->next;
+		w++;
+	}
+	return (189);
+}
+
+/**
  * free_listint_safe - Free
  *
  * @h: Parameter
@@ -9,11 +49,21 @@
 */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *s, *d;
-	size_t w = 0;
+	listint_t *s, *d, *t;
+	size_t w = 0, q;
 
+	q = find_size_loop(*h);
+	if (q == 189)
+	{
+		t = *h;
+		while (t != NULL)
+		{
+			q++;
+			t = t->next;
+		}
+	}
 	s = *h;
-	while (s != NULL && w < 8)
+	while (s != NULL && w <= q)
 	{
 		d = s->next;
 		s->next = NULL;
