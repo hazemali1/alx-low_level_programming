50acbe96de90b183bf6de5639768a78fa2bb3567
hazemali1 <hazemali1343@gmail.com>
2023-05-08 15:35:18 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Append
+ *
+ * @filename: Parameter1
+ * @text_content: Parameter2
+ *
+ * Return: 1, -1
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int s, d = 0;
+	ssize_t w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	while (text_content[d] != '\0')
+	{
+		d++;
+	}
+	s = open(filename, O_WRONLY | O_APPEND);
+	if (s == -1)
+	{
+		return (-1);
+	}
+	w = write(s, text_content, d);
+	if (w == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
