fd6c6e6122b25b9c777db875df597a1c51dd300b
hazemali1 <hazemali1343@gmail.com>
2023-05-08 15:39:44 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,6 +26,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
 	w = write(s, text_content, d);
 	close(s);
 	if (w == -1)
