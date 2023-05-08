328604fe7c8e931fcbd04673132d81781dbe40f1
hazemali1 <hazemali1343@gmail.com>
2023-05-08 14:09:56 -0700

    Write on screen from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - Write output
+ *
+ * @filename: PArameter1
+ * @letters: Parameter2
+ *
+ * Return: Lenght
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int s, d;
+	char w[10000];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	s = open(filename, O_RDONLY);
+	if (s == -1)
+	{
+		return (0);
+	}
+	d = read(s, w, letters);
+	write(STDOUT_FILENO, w, d);
+	return (d);
+}
