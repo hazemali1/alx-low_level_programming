45399dbb96391dd9122b4030fa433fdf2c605fd9
hazemali1 <hazemali1343@gmail.com>
2023-05-08 17:49:41 -0700

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * main - Main
+ *
+ * @argc: Parameter1
+ * @argv: Parmeter2
+ *
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int s, d, w, q, e, r;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	s = open(argv[1], O_RDONLY);
+	q = read(s, buff, 1024);
+	if (s == -1 || q == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	d = open(argv[2], O_CREAT | O_WRONLY |O_TRUNC | O_APPEND, 0664);
+	w = write(d, buff, q);
+	if (d == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	e = close(s);
+	r = close(d);
+	if (e == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", s);
+		exit(100);
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", d);
+		exit(100);
+	}
+	return (0);
+}
