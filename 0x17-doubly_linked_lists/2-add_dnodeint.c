c901df0786b3a7b3819246b8447c89ba988c88da
hazemali1 <hazemali1343@gmail.com>
2023-06-15 09:56:37 -0700

    add at beging

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Add at beging
+ *
+ * @heaad: Parameter1
+ * @n: PArametrer2
+ *
+ * Return: Pointer
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *s;
+
+	s = malloc(sizeof(dlistint_t));
+	if (s == NULL)
+		return (NULL);
+	s->n = n;
+	s ->next = *head;
+	s->prev = NULL;
+	*head = s;
+	return (s);
+}
