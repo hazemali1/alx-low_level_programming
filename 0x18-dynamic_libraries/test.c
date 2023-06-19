804425c9fc35a824671713e4d4c38a08f606a540
hazemali1 <hazemali1343@gmail.com>
2023-06-19 08:42:25 -0700

    giga millions

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * printf - Printf function
+ *
+ * @format: PArmaeter
+ *
+ * Return: Exit
+*/
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 55);
