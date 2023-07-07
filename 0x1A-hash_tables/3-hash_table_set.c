5fc7981d412826e1c32e1f4fc2e151acee463dd0
hazemali1 <hazemali1343@gmail.com>
2023-07-07 06:22:40 -0700

    insert

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,23 +5,33 @@
  *
  * @key: Parameter1
  * @value: Parameter2
+ * @ht: Parameter3
  *
  * Return: 1 or 0
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int i;
+	unsigned long int i, j;
 	hash_node_t *hash;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	for (j = i; ht->array[j]; j++)
+	{
+		if (strcmp(ht->array[j]->key, key) == 0)
+		{
+			free(ht->array[j]->value);
+			ht->array[j]->value = strdup(value);
+			return (1);
+		}
+	}
 	hash = malloc(sizeof(hash_node_t));
 
 	if (hash == NULL)
 		return (0);
 	hash->key = strdup(key);
 	hash->value = strdup(value);
-	i = key_index((const unsigned char *)key, ht->size);
 	hash->next = ht->array[i];
 	ht->array[i] = hash;
 	return (1);
