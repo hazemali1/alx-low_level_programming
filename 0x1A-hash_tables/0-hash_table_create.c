3460deac95cbe10c3bd5a6ba9011ddda683cefb2
hazemali1 <hazemali1343@gmail.com>
2023-07-07 04:43:12 -0700

    create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
+
 	if (hash == NULL)
 		return (NULL);
 	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t) * size);
+	if (hash->array == NULL)
+		return (NULL);
 	return (hash);
 }
