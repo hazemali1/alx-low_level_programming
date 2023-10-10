bdb92c7b08261a1270180173f0cd690ffbd7fb9f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-10 22:34:40 +0300

    binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Binary Serach
+ * @array: Array
+ * @size: Size of array
+ * @value: Value looking for
+ * Return: return index
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, s;
+	int *arr, d;
+
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+	if (size % 2 == 0)
+		s = (size / 2) - 1;
+	else
+		s = size / 2;
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
+	{
+		printf(" %d", array[i]);
+		if (i + 1 == size)
+			printf("\n");
+		else
+			printf(",");
+	}
+	if (array[s] == value)
+		return (s);
+	else if (value < array[s])
+	{
+		return (binary_search(array, s, value));
+	}
+	else
+	{
+		arr = array + s + 1;
+		d = binary_search(arr, size - s - 1, value);
+		if (d != -1)
+			return (d + s + 1);
+		else
+			return (d);
+	}
+	return (-1);
+}
