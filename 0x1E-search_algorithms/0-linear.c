711501740abc8d91a07df427eef090fb1b8b8229
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-10 19:38:16 +0300

    liner search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear Serach
+ * array: Array
+ * size: Size of array
+ * value: Value looking for
+ * Return: return index
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t s;
+
+	if (array == NULL)
+		return (-1);
+
+	for (s = 0; s < size; s++)
+	{
+		printf("Value checked array[%d] = [%d]", s, array[s]);
+		if (array[s] == value)
+			return (s);
+	}
+	return (-1);
+}
