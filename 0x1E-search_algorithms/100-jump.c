ebc8cc0c5d9824c9f5a44d97a70ab4dace185d93
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-11 21:00:59 +0300

    jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i += s)
 	{
 		if (array[i] < value)
-			printf("Value checked array[%ld] = [%ld]\n", i, i);
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		else
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", i - s, i);
@@ -35,7 +35,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 	for (i = f; i <= l; i++)
 	{
-		printf("Value checked array[%ld] = [%ld]\n", i, i);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
