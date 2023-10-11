aef59b7c1c176366bbfe268a3c4f139443a93731
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-11 21:57:21 +0300

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Interpolation Serach
+ * @array: Array
+ * @size: Size of array
+ * @value: Value looking for
+ * Return: return index
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i;
+	size_t pos = 0 + (((double)((size - 1) - 0) / (array[size - 1] - array[0])) * (value - array[0]));
+
+	if (array == NULL)
+		return (-1);
+	for (i = pos; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	if (pos > size)
+		printf("Value checked array[%ld] is out of range\n", pos);
+	return (-1);
+}
